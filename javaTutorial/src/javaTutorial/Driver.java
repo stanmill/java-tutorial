63f0de927e581654da882ce6b436886e3669008b
stanmill <stanley.pena29@gmail.com>
2019-09-15 15:54:37 -0400

    Update Driver.java

--- javaTutorial/src/javaTutorial/Driver.java
+++ javaTutorial/src/javaTutorial/Driver.java
@@ -5,7 +5,7 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello Wrold!");
+		System.out.println("Hello World!");
 		System.out.println("hello from universe 7");
 
 	}
