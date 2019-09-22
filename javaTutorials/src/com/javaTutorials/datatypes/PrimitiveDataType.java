c8530309b4b773e7c9951d41b96ac1ec9d050780
stanmill <stanley.pena29@gmail.com>
2019-09-22 17:52:38 -0400

    update

--- javaTutorials/src/com/javaTutorials/datatypes/PrimitiveDataType.java
+++ javaTutorials/src/com/javaTutorials/datatypes/PrimitiveDataType.java
@@ -3,7 +3,13 @@ package com.javaTutorials.datatypes;
 public class PrimitiveDataType {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int number = 10;
+		int sum = 10 + number;
+		
+		double d = 10;
+		float f = 10.9f;
+		
+		System.out.printf("%.3f", f);
 
 	}
 
