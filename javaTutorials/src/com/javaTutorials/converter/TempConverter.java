c68831cc440819bf5e2229da83cdd41f100b13ba
stanmill <stanley.pena29@gmail.com>
2019-09-29 19:44:39 -0400

    temp project

---
+++ javaTutorials/src/com/javaTutorials/converter/TempConverter.java
@@ -0,0 +1,28 @@
+package com.javaTutorials.converter;
+
+public class TempConverter {
+	
+	public static void main (String[] args) {
+		
+		// Temperature formula T(F) = T(C) * 1.8 +32
+		double c = 12;
+		double f = c * 1.8 + 32;
+
+			
+		System.out.println(c+" degrees celcius is "+f+" fahrenheit");
+		
+		// Primitive data types
+		char ch = 'f';
+		boolean bool = true;
+		// Java class
+		String s = "sjdlf";
+		
+		System.out.println(ch+" this is a character");
+		System.out.println(bool);
+		System.out.println(s);
+
+		
+		
+	}
+
+}
