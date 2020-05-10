42ebab31e6123cea7e752b25469db8e069c92c9c
stanmill <stanley.pena29@gmail.com>
2020-05-10 12:17:54 -0700

    while loops

---
+++ javaTutorials/src/com/javaTutorials/loops/WhileLoop.java
@@ -0,0 +1,17 @@
+package com.javaTutorials.loops;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		int i = 0;
+		boolean state = true;
+		
+		while(1 < 10) {
+			System.out.println(i);
+			break;
+		}
+
+	}
+
+}
